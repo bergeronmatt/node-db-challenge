768e1e70cedf8336f85554baf6967993377b4320
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-17 15:58:23 -0400

    mvp

--- post-router/router-model.js
+++ post-router/router-model.js
@@ -3,10 +3,72 @@ const db = require('../data/db-config');
 
 //helper functions
 
+/** PROJECT FUNCTIONS */
+
+// get all projects
 function getProjects () {
     return db('projects');
 }
 
+// get project by id
+function findProject (id){
+    return db('projects').where({id}).first()
+}
+
+// add new project
+function addProject(project){
+    return db('projects')
+        .insert(project, 'id')
+        .then(([id]) => {
+            return findProject(id)
+        })
+}
+
+
+/** TASKS FUNCTIONS */
+
+// get task associated with a project
+function getTasks (id) {
+    return db('projects AS p')
+        .join('tasks AS t', 't.project_id', 'p.id')
+        .select('p.name', 't.description', 't.notes')
+        .where('p.id', id)
+}
+
+// find specfic task by id
+function findTask(id){
+    return db ('tasks').where({id}).first()
+}
+
+//add new task
+function addTask(task){
+    return db('tasks')
+        .insert(task)
+        .then(ids => ({id : ids[0]}))
+}
+
+/** RESOURCE FUNCTIONS */
+
+// get resource list
+
+function getResources () {
+    return db('resources');
+}
+
+// add resource
+function addResources (resource) {
+    return db("resources ")
+        .insert(resource)
+        .then(ids => ({ id: ids[0] }));
+}
+
 module.exports = {
-    getProjects
+    getProjects,
+    addProject,
+    findProject,
+    getTasks,
+    addTask,
+    findTask,
+    getResources,
+    addResources
 }
--- post-router/router.js
+++ post-router/router.js
@@ -16,12 +16,95 @@ router.get('/', (req, res) =>{
             if(!projects){
                 res.status(404).json({ error: 'Could not find projects list'})
             } else {
+                console.log(projects)
                 res.status(201).json({ message: 'Rendering Project list...', projects})
             }
         })
         .catch(err => {
             res.status(500).json({ errorMessage: 'Could not retrieve list of projects'})
         })
+})
+
+
+//read the list of tasks associated with a project
+
+router.get('/:id/tasks', (req, res) => {
+    const id = req.params.id;
+    Projects.findProject(id)
+        .then(project => {
+            if(!project){
+                res.status(404).json({error: "The project for the task search could not be identified by the given id, please try again."})
+            } else {
+                Projects.getTasks(id)
+                    .then(task => {
+                        res.status(201).json({message: 'loading task', task})
+                    })
+            }
+        })
+        .catch(err =>{
+            res.status(500).json({errorMessage: 'Could not render the project for the task search.'})
+        })
+})
+
+//read the list of resources
+
+router.get('/resources', (req, res) => {
+    Projects.getResources()
+        .then(resources => {
+            if(!resources){
+                res.status(404).json({ error: 'Could not find resources list'})
+            } else {
+                console.log(resources)
+                res.status(201).json({ message: 'Rendering Resource list...', resources})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Could not retrieve list of projects'})
+        })
+})
+
+/** CREATE */
+
+//create project
+
+router.post('/', (req, res) => {
+    const newProject = req.body
+    if(!newProject){
+        res.status(400).json({ error: 'Project alread exists'})
+    } else {
+        Projects.addProject(newProject)
+            .then(project => {
+                res.status(201).json({ message: 'Project added', project})
+            })
+            .catch(err =>{
+                res.status(500).json({errorMessage: 'The project could not be saved to the database.', err})
+            })
+    }
+})
+
+//create task to a specific project
+
+router.post('/:id/task', (req, res) => {
+    const body = req.body;
+    Projects
+        .addTask(body)
+        .then((add) => {
+            res.status(201).json(add);
+        }).catch((err) => {
+            res.status(500).json({ message: "There was an error adding a task" })
+        });
+})
+
+//create resource
+
+router.post('/resources', (req, res) => {
+    const body = req.body;
+    Projects.addResources(body)
+        .then((add) => {
+            res.status(201).json(add);
+        }).catch((err) => {
+            res.status(500).json({ message: "There was an error adding a resource" })
+        });
 })
 
 module.exports = router;
