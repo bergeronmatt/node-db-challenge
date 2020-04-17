0c35de7a5f65d4daee80c0c06293978ea27f6f2b
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-17 12:15:06 -0400

    added in the index code

---
+++ index.js
@@ -0,0 +1,10 @@
+//set up server object
+const server = require('./server');
+
+//set up PORT object
+const PORT = process.env.PORT || 5000;
+
+//listen to server
+server.listen(PORT, () => {
+    console.log(`\n===API RUNNING ON http://localhost:${PORT}===\n`)
+});
